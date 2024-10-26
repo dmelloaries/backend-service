6ab8bc4bb56db48179c20145b791db4bdc6722ba
Aries Dmello <dmelloaries@gmail.com>
2024-10-26 12:53:12 +0530

    cors2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const phoneRoutes = require("./routes/phoneRoutes");
 
 dotenv.config();
 const app = express();
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = ["http://localhost:3000","https://intelliflow-three.vercel.app"];
 
 app.use(
   cors({
