a8687b97a7de4bd7833c096c99e8411406868f61
Aries Dmello <dmelloaries@gmail.com>
2024-10-26 06:38:56 +0530

    cors fixed

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // server.js
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/database');
-const phoneRoutes = require('./routes/phoneRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/database");
+const phoneRoutes = require("./routes/phoneRoutes");
 
 dotenv.config();
 
+const allowedOrigins = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204,
+  })
+);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -16,7 +27,7 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.use('/api', phoneRoutes);
+app.use("/api", phoneRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
